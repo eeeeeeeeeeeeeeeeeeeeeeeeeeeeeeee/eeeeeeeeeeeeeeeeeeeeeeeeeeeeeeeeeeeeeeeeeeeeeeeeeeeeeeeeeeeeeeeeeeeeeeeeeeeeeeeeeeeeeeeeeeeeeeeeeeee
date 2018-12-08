47c6f0afa4e96899e0180b6e59ecfc29431ce636
Adriano Candido Camilo <cobkarter@gmail.com>
2018-12-08 14:40:22 -0200

    Update e.cs

--- e.cs
+++ e.cs
@@ -1,12 +1,26 @@
 using System;
+using System.Threading;
 
-namespace e
+public class Program
 {
-    class E
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine('e');
+            for (int e = 0; e < E.Length; e++)
+            {
+                Console.Write(new string('E', E[e]));
+
+                Console.ForegroundColor = Console.ForegroundColor >= ConsoleColor.White ?
+                Console.ForegroundColor = ConsoleColor.DarkBlue :
+                Console.ForegroundColor + 1;
+                Console.WriteLine();
+            }
+
+            Thread.Sleep(0xEE);
+            Console.Clear();
         }
     }
+
+    private static int[] E = new int[] { 19, 19, 19, 3, 3, 3, 3, 3, 19, 19, 19, 3, 3, 3, 3, 3, 19, 19, 19, };
 }
