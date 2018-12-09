4f092f967be5dfb511b2e63f86fde2c1dcfe039a
Takumi Sueda <puhitaku@gmail.com>
2018-12-10 01:21:54 +0900

    Create ebig.py

---
+++ ebig.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+exec('''
+         'eeeeee'\
+      'eeee'\nimport\
+    sys\n'    '\nwhile\
+   True:        \n print\
+  ('eee'         'eeeee'\
+ 'eeeeeeeeeeeeeeeeeeeee'\
+ 'eeee'\
+  'eeeee'        'eeeee'\
+   'eeeeeeeeeeeeeeeee'\
+       'ee',end='')
+'''[10:])
