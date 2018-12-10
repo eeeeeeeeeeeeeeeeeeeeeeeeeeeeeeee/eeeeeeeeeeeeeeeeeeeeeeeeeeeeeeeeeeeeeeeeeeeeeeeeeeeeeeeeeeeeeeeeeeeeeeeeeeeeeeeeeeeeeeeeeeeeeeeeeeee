8237bb964e6fc59d5dc83cc7e719d7dabb3c9c88
Sero Wong <minecraft31317015@gmail.com>
2018-12-10 20:07:00 +0800

    Added e.hpp as header, e-ed to the next level.

--- e.cpp
+++ e.cpp
@@ -1,4 +1,11 @@
+//Compile with e.hpp please.
+//g++ e.cpp,e.hpp -o e.exe
+
 #include <iostream>
+#ifndef EEE
+    #include <e.hpp>
+#endif
+
 #define e using
 #define ee namespace
 #define eee std
@@ -7,23 +14,33 @@
 #define eeeeee main
 #define eeeeeee (
 #define eeeeeeee )
-#define eeeeeeeee while
-#define eeeeeeeeee true
-#define eeeeeeeeeee {
-#define eeeeeeeeeeee }
-#define eeeeeeeeeeeee cout
-#define eeeeeeeeeeeeee cerr
-#define eeeeeeeeeeeeeee <<
-#define eeeeeeeeeeeeeeee 'e'
-#define eeeeeeeeeeeeeeeee return
+#define eeeeeeeee {
+#define eeeeeeeeee }
+#define eeeeeeeeeee E
+#define eeeeeeeeeeee EE
+#define eeeeeeeeeeeee while
+#define eeeeeeeeeeeeee true
+#define eeeeeeeeeeeeeee cout
+#define eeeeeeeeeeeeeeee cerr
+#define eeeeeeeeeeeeeeeee <<
+#define eeeeeeeeeeeeeeeeee .
+#define eeeeeeeeeeeeeeeeeee endl
+#define eeeeeeeeeeeeeeeeeeee return
+#define eeeeeeeeeeeeeeeeeeeee class
+#define eeeeeeeeeeeeeeeeeeeeee public
+#define eeeeeeeeeeeeeeeeeeeeeee private
+#define eeeeeeeeeeeeeeeeeeeeeeee :
+#define eeeeeeeeeeeeeeeeeeeeeeeee char
+#define eeeeeeeeeeeeeeeeeeeeeeeeee =
+#define eeeeeeeeeeeeeeeeeeeeeeeeeee 'e'
 
 e ee eee eeee
-eeeee eeeeee eeeeeee eeeeeeee
-eeeeeeeeeee
-eeeeeeeee eeeeeee eeeeeeeeee eeeeeeee
-eeeeeeeeeee
-eeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeeee eeee
-eeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeeee eeee
-eeeeeeeeeeee
-eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeee eeee
-eeeeeeeeeeee
+
+eeeee eeeeee eeeeeee eeeeeeee eeeeeeeee
+    eeeeeeeeeee eeeeeeeeeeee eeee
+    eeeeeeeeeeeee eeeeeee eeeeeeeeeeeeee eeeeeeee eeeeeeeee
+        eeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeee eeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeee eeee
+        eeeeeeeeeeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeee eeeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeee eeee
+    eeeeeeeeee
+    eeeeeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeee eeeeeeee eeee
+eeeeeeeeee
