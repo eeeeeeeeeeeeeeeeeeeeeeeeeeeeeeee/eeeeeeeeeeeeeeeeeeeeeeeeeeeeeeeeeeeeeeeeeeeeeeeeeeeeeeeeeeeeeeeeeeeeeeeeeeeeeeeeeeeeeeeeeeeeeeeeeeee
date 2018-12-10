b48052885f1a16eb883eaaa680d4a2520771cb16
Sero Wong <minecraft31317015@gmail.com>
2018-12-10 20:10:10 +0800

    For the new e.cpp, the e (key) to open the e (gate) of the new e (world).

---
+++ e.hpp
@@ -0,0 +1,43 @@
+//Compile with e.hpp please.
+//g++ e.cpp,e.hpp -o e.exe
+
+#ifndef EEE
+    #define EEE
+
+    #define e using
+    #define ee namespace
+    #define eee std
+    #define eeee ;
+    #define eeeee int
+    #define eeeeee main
+    #define eeeeeee (
+    #define eeeeeeee )
+    #define eeeeeeeee {
+    #define eeeeeeeeee }
+    #define eeeeeeeeeee E
+    #define eeeeeeeeeeee EE
+    #define eeeeeeeeeeeee while
+    #define eeeeeeeeeeeeee true
+    #define eeeeeeeeeeeeeee cout
+    #define eeeeeeeeeeeeeeee cerr
+    #define eeeeeeeeeeeeeeeee <<
+    #define eeeeeeeeeeeeeeeeee .
+    #define eeeeeeeeeeeeeeeeeee endl
+    #define eeeeeeeeeeeeeeeeeeee return
+    #define eeeeeeeeeeeeeeeeeeeee class
+    #define eeeeeeeeeeeeeeeeeeeeee public
+    #define eeeeeeeeeeeeeeeeeeeeeee private
+    #define eeeeeeeeeeeeeeeeeeeeeeee :
+    #define eeeeeeeeeeeeeeeeeeeeeeeee char
+    #define eeeeeeeeeeeeeeeeeeeeeeeeee =
+    #define eeeeeeeeeeeeeeeeeeeeeeeeeee 'e'
+
+    eeeeeeeeeeeeeeeeeeeee eeeeeeeeeee eeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeee
+            eeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeee eeeeeeee eeeeeeeee
+                eeeeeeeeeeeeeeeeeeee eeeeeeeeeee eeee
+            eeeeeeeeee
+        eeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeee
+            eeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeee eeee
+    eeeeeeeeee eeee
+#endif
