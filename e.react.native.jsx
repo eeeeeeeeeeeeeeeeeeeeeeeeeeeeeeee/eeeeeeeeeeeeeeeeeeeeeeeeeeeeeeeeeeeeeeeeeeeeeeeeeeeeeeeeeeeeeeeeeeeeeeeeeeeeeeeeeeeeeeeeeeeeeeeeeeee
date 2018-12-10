feb120b854983ee570aee7d5908835bf8a895a55
BAYU KRESNO PRASETYO <contact@bayukresno.id>
2018-12-10 16:38:30 +0700

    React Native can do eeeeeeeeeeeeee
    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ e.react.native.jsx
@@ -0,0 +1,11 @@
+import React, { AppRegistry, Text, PureComponent } from 'react-native';
+
+class E extends PureComponent {
+  render() {
+    return (
+      <Text>eeeeeeeeeeeeeeeeeeeeeeeeee</Text>
+    )
+  }
+}
+
+AppRegistry.registerComponent('eeeeeee', () => E);
