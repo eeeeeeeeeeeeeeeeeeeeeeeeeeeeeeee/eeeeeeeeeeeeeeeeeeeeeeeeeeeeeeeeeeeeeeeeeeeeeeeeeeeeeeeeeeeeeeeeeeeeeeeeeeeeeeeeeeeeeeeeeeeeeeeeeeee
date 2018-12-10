6932e61bc28510456dbee39d924e82adfe5d79fb
iosmanthus <myosmanthustree@gmail.com>
2018-12-10 10:12:53 +0800

    add functional feature

--- e.rs
+++ e.rs
@@ -1,7 +1,15 @@
-#![feature(main)]
-#[main]
+#[cfg(feature = "functional")]
+fn e() {
+    std::iter::repeat('e').for_each(|e| print!("{}", e))
+}
+
+#[cfg(not(feature = "functional"))]
 fn e() {
     loop {
         print!("e");
     }
+}
+
+fn main() {
+    e();
 }
