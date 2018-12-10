93ea3181944866b3a2c7c2938946458bd3bf3883
rurimo <rurimo@naver.com>
2018-12-10 13:27:55 +0900

    e_android

---
+++ e_android/app/src/main/java/eee/eeeeeeee/eeeeeeeeeeeeeee/MainEEEEEEEEEEEEEEE.kt
@@ -0,0 +1,17 @@
+package eee.eeeeeeee.eeeeeeeeeeeeeee
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainEEEEEEEEEEEEEEE : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eeeeeeeee)
+
+        while(true) {
+            Toast.makeText(this, "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
