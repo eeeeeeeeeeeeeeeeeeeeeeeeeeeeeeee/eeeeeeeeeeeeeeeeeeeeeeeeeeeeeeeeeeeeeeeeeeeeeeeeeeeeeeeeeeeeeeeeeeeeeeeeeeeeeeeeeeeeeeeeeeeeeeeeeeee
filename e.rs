fb1378ba878f08439e7374e393e99e1141adc517
iosmanthus <myosmanthustree@gmail.com>
2018-12-10 18:20:51 +0800

    marking fn e as #[main]

--- e.rs
+++ e.rs
@@ -1,15 +1,14 @@
+#![feature(main)]
 #[cfg(feature = "functional")]
+#[main]
 fn e() {
-    std::iter::repeat('e').for_each(|e| print!("{}", e))
+    std::iter::repeat('e').for_each(|e| print!("{}", e));
 }
 
 #[cfg(not(feature = "functional"))]
+#[main]
 fn e() -> ! {
     loop {
         print!("e");
     }
-}
-
-fn main() {
-    e();
 }
