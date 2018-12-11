56b317eaf24f31c7e756e08f53f790f9fe953e69
Mariell <proximyst@proximyst.com>
2018-12-11 18:00:13 +0100

    make e.c follow e.cpp's example

--- e.c
+++ e.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#define e "e"
+#define ee int
+#define eee main
+#define eeee (
+#define eeeee )
+#define eeeeee {
+#define eeeeeee }
+#define eeeeeeee for
+#define eeeeeeeee ;
+#define eeeeeeeeee printf
+#define eeeeeeeeeee return
+#define eeeeeeeeeeee on_exit
+#define eeeeeeeeeeeee [
+#define eeeeeeeeeeeeee ]
+#define eeeeeeeeeeeeeee 0
 
-int main()
-{
-	for (;;) {
-		printf("e");
-	}
-}
-
-int on_exit()
-{
-	return 'e';
-}
+ee eee eeee eeeee eeeeee eeeeeeee eeee eeeeeeeee
+eeeeeeeee eeeee eeeeee eeeeeeeeee eeee e eeeee
+eeeeeeeee eeeeeee eeeeeee ee eeeeeeeeeeee eeee
+eeeee eeeeee eeeeeeeeeee e eeeeeeeeeeeee
+eeeeeeeeeeeeeee eeeeeeeeeeeeee eeeeeeeee eeeeeee
